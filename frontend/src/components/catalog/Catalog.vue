6cd99d02873f6c9daeef55b6864d3d401f7e5122
Fabian Hagen <fabian@faaren.com>
2024-06-10 19:56:11 +0000

    Alglia Base-Config

---
+++ frontend/src/components/catalog/Catalog.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <ais-instant-search :search-client="searchClient" index-name="products">
+
+            <ais-dynamic-widgets>
+                <ais-refinement-list attribute="categories" :show-more="true" />
+            </ais-dynamic-widgets>
+
+            <ais-hits>
+                <template v-slot:item="{item}">
+                    <h2>{{ item.name }}</h2>
+                </template>
+            </ais-hits>
+        </ais-instant-search>
+    </div>
+</template>
+
+<script setup>
+import algoliasearch from 'algoliasearch/lite'
+import 'instantsearch.css/themes/algolia-min.css'
+
+const searchClient = algoliasearch(
+    '42ASY60BGE',
+    '924ec83ace068ea172eac93f844ee954'
+)
+
+</script>
+
+<style scoped>
+body {
+  font-family: sans-serif;
+  padding: 1em;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import InstantSearch from 'vue-instantsearch/vue3/es'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(InstantSearch)
+
+app.mount('#app')
